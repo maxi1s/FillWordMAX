5a8a7b0a77cdb1021913cdbadb76e51cf510a36b
Max <magmacube@mail.ru>
2025-05-08 14:34:11 +0400

    fix

---
+++ app/src/main/java/com/fillwordmax/auth/AuthState.kt
@@ -0,0 +1,10 @@
+package com.fillwordmax.auth
+
+import com.google.firebase.auth.FirebaseUser
+
+sealed class AuthState {
+    object Loading : AuthState()
+    data class Authenticated(val user: FirebaseUser) : AuthState()
+    object Unauthenticated : AuthState()
+    data class Error(val message: String) : AuthState()
+} 
